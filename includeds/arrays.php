05ec64107805b3579fd4b600314a8366f93c7eca
Zsolti426 <62592162+Zsolti426@users.noreply.github.com>
2021-02-08 15:43:53 +0100

    Arrays.php fájl elkészítése és feltöltése
    Elkészítettem és feltölöttem az Arrays.php fájlt

---
+++ includeds/arrays.php
@@ -0,0 +1,72 @@
+<?php
+
+    $navItems = array(
+                    array(
+                        'slug' => "index.php",
+                        'title' => "Home"
+                    ),
+                    array(
+                        'slug' => "team.php",
+                        'title' => "Team"
+                    ),
+                    array(
+                        'slug' => "menu.php",
+                        'title' => "Menu"
+                    ),
+                    array(
+                        'slug' => "contact.php",
+                        'title' => "Contact"
+                    ),
+    );
+
+    $teamMembers = array(
+                    array(
+                        'name' => "Frankie",
+                        'position' => "Owner",
+                        'bio' => "Insert team-member boigraphy here",
+                        'img' => "frankie"
+                    ),
+                    array(
+                        'name' => "Francis",
+                        'position' => "General manager",
+                        'bio' => "Insert team-member boigraphy here",
+                        'img' => "francis"
+                    ),
+                    array(
+                        'name' => "Carlos",
+                        'position' => "Chef",
+                        'bio' => "Insert team-member boigraphy here",
+                        'img' => "carlos"
+                    )
+    );
+
+    $menuItems = array(
+        "sandwitch" => array(
+            'title' => "Sandwitch",
+            'price' => 11,
+            'blurb' => "Sandwitch description here",
+            'drink' => "Suggested ital here"
+        ),
+
+        "dill-salmon" => array(
+            'title' => "Lemon &amp; Dill salmon",
+            'price' => 18,
+            'blurb' => "Food description here",
+            'drink' => "Suggested ital here"
+        ),
+        "super-salad" => array(
+            'title' => "Super salad<sup>&reg;</sup>",
+            'price' => 34,
+            'blurb' => "Salad description here",
+            'drink' => "Suggested ital here"
+        ),
+        "mexican" => array(
+            'title' => "Mexican Barbaoca",
+            'price' => 23,
+            'blurb' => "Mexican description here",
+            'drink' => "Suggested ital here"
+        ),
+
+
+    );
+?>
