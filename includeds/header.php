287d78e5464bf5c119582b58bbe06c880cbf0f78
Kanyok Dávid <71319711+KanyokDavid@users.noreply.github.com>
2021-02-08 15:42:23 +0100

    Header php létrehozása és feltöltése!
    Header php létrehozása és feltöltése!

---
+++ includeds/header.php
@@ -0,0 +1,19 @@
+<?php include('includes/arrays.php'); ?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo TITLE;?></title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <a href="/" title="Return to home">
+                    <img src="img/banner.png" alt="Banner">
+                </a>
+            </header>
+            <nav>
+                <?php include('includes/nav.php');?>
+            </nav>
+            <article>
