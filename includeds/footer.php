541ea370fea4bebea8565628c58d013281cc87bb
Kanyok Dávid <71319711+KanyokDavid@users.noreply.github.com>
2021-02-08 15:41:59 +0100

    Footer php - létrehozása és feltöltése
    Footer php - létrehozása és feltöltése

---
+++ includeds/footer.php
@@ -0,0 +1,33 @@
+
+            </article>
+            
+            <div class="end-content">
+                <footer class="cf">
+                    <div class="column three">
+                        <strong>Phone</strong>
+                        808.529.3814
+                    </div>
+                    <div class="column three">
+                        <strong>Location</strong>
+                        123 Kapoliana Blv
+                        Honolulu, HI
+                    </div>
+                    <div class="column three last">
+                        <strong>Hours</strong>
+
+                        <em>Tuesday - Thursday</em>
+                        1:00pm- 9:00pm <br><br>
+
+                        <em>Friday - Saturday</em>
+                        4:00pm- 11:00pm <br><br>
+
+                        <em>Sunday - Monday</em>
+                        Closed<br>
+                    </div>
+                </footer>
+
+                <small>&copy;<?php echo date('Y') ?> Bakos Bence</small>
+            </div>
+        </div>
+    </body>
+</html>
