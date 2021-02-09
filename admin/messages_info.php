492a2f15809d269a1b2d5f676ae4e4a501aa7ba2
VLGJBZ <61667095+VLGJBZ@users.noreply.github.com>
2021-02-09 12:44:45 +0100

    messages_info.php javítása
    messages_info.php javítása

--- admin/messages_info.php
+++ admin/messages_info.php
@@ -11,15 +11,20 @@
 		$con->set_charset("UTF8");
 ?>
 
+<H1>Messages list!</H1>
+<form method="post">
+    <input type="text" name="search" placeholder="Keresés a táblában">
+    <input type="submit" value="Keresés" name="keres">
 </form>
-<table>
-<tr>
-<td>Nev</td>
-<td>Email</td>
-<td>Message</td>
-<td>Edit</td>
-<td>Delete</td>
-</tr>
+	</form>
+	<table>
+	<tr>
+		<td>Nev</td>
+		<td>Email</td>
+		<td>Message</td>
+		<td>Edit</td>
+		<td>Delete</td>
+	</tr>
 
 
 <?php
@@ -28,18 +33,18 @@ $result=mysqli_query($con, $query);
 while ($row=mysqli_fetch_assoc($result))
 {
 echo 
-'<tr>
-<td>'.$row["name"].'</td> 
-<td>'.$row["email"].'</td>
-<td>'.$row["message"].'</td>
-<td>
-<a href="messages_edit.php?edit='.$row["id"].'">
-<input type="submit" name="submit" value="Edit"></a></td>
-<td>
-<form action="messages_info.php" method="post">
-<input type="hidden" name="id" value="'.$row["id"].'">
-<input type="submit" name="submit" value="Delete">
-</form></td></tr>';
+	'<tr>
+		<td>'.$row["name"].'</td> 
+		<td>'.$row["email"].'</td>
+		<td>'.$row["message"].'</td>
+	<td>
+		<a href="messages_edit.php?edit='.$row["id"].'">
+		<input type="submit" name="submit" value="Edit"></a></td>
+	<td>
+	<form action="messages_info.php" method="post">
+		<input type="hidden" name="id" value="'.$row["id"].'">
+		<input type="submit" name="submit" value="Delete">
+	</form></td></tr>';
 }
 if(isset($_POST["submit"]))
 {
