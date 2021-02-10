4f9d70ece70e3abb37699441893b16cd4936f266
Zsolti426 <62592162+Zsolti426@users.noreply.github.com>
2021-02-10 10:16:15 +0100

    Login.php elkészítése és módosítása
    Módosítottam a login.php tartalmát

--- admin/login.php
+++ admin/login.php
@@ -1,22 +1,126 @@
-<?php 
-    include('admin_includes/database.php');
-    session_start();
+<?php
+$username="root";
+$passwd="";
+$dbname="qnszt_gyak";
+$szerver="localhost";
+$con=mysqli_connect($szerver,$username,$passwd,$dbname);
+$con->set_charset("UTF8");
+session_start();if(isset($_POST['submit'])){
+	if(isset($_POST['captcha_challenge']) && $_POST['captcha_challenge'] == $_SESSION['captcha_text']){
+		$query="select * from admin where name='".$_POST['name']."'";
+		$result=mysqli_query($con,$query);
+		if($sor=mysqli_fetch_assoc($result)){
+			$passwordel=password_verify($_POST['password'],$sor['password']);
+			if($passwordel==false){
+				echo"<script>alert('Nem jó a jelszó!')</script>";
+				header('Refresh:0');
+			exit();
+			}
+			else{
+				$_SESSION['name']=$sor['name'];
+				echo '<meta http-equiv="refresh" content="0;url=index.php">';
+			}
+		}
+	}	
+}
 ?>
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title><?php echo TITLE;?></title>
-        <link rel="stylesheet" href="../style.css">
-    </head>
-    <body>
-        <div class="wrapper">
-            <header>
-                <img src="../img/banner.png" alt="Banner">
-            </header>
-            <article>
-                
-            </article>
-        </div>
-    </body>
-</html>
+<div class="form">
+	<form action="login.php" method="post" enctype="multipart/form-data">
+				<label for="name">Name:</label>
+				<input type="text" name="name" placeholder="Név" required><br>
+				<label for="password">Password: </label><br>
+				<input type="password" name="password" placeholder="Jelszó" required><br>
+	    	<br>
+				<input type="submit" value="Belépés" name="submit"></br>
+				<a href="index.php">Back</a>
+	</form>			
+			
+			
+</div>
+<style>
+input[type=text]{
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=password]{
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #d3d3d3;
+  color: black;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button{
+	width: 100%;
+	background-color: #d3d3d3;
+	color: black;
+	padding: 14px 20px;
+	margin: 8px 0;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+}
+
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.form {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  position: sticky;
+  position: -webkit-sticky;
+  top: 0;
+}
+
+/* Style the navigation bar links */
+.navbar a {
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 20px;
+  text-decoration: none;
+}
+
+
+/* Right-aligned link */
+.navbar a.right {
+  float: right;
+}
+
+/* Change color on hover */
+.navbar a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Active/current link */
+.navbar a.active {
+  background-color: #666;
+  color: white;
+}
+</style>
